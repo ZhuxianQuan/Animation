a20f39f74d296dd272036c175ba0773a08cb1617
Big Shark <bigshark0810@hotmail.com>
2017-02-10 13:46:54 -0800

    Delete an unnecessary project

--- Alarm For Children/Pods/Target Support Files/Pods-Alarm For Children/Pods-Alarm For Children-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Alarm_For_Children : NSObject
-@end
-@implementation PodsDummy_Pods_Alarm_For_Children
-@end
--- Alarm For Children/Pods/Target Support Files/Pods-Alarm For Children/Pods-Alarm For Children-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_Alarm_For_ChildrenVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Alarm_For_ChildrenVersionString[];
-
